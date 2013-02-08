33fda9bce6643816128c61778b8d41aa97b8a451
Stephen <stephenkamenar@gmail.com>
2013-02-07 23:49:17 -0500

    init

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
+var http = require('http');
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port);
+console.log('Server running at http://127.0.0.1:port/');
