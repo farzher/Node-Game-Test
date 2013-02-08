34ef3d7c30b40d72c15047895642892dad308337
Stephen <stephenkamenar@gmail.com>
2013-02-07 23:44:20 -0500

    init

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var app = require('http').createServer(handler),
-app.listen(process.env.PORT || 80);
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
